e1a957df25dc9accbae1174cd0ee29f0695a9f4b
Eugene Lin <eugenelin89@Eugenes-MacBook-Pro.local>
2013-11-16 14:30:06 -0800

    basket done, i think..

--- src/main/java/edu/umn/cs/recsys/ii/SimpleGlobalItemScorer.java
+++ src/main/java/edu/umn/cs/recsys/ii/SimpleGlobalItemScorer.java
@@ -3,10 +3,12 @@ package edu.umn.cs.recsys.ii;
 import org.grouplens.lenskit.basic.AbstractGlobalItemScorer;
 import org.grouplens.lenskit.scored.ScoredId;
 import org.grouplens.lenskit.vectors.MutableSparseVector;
+import org.grouplens.lenskit.vectors.VectorEntry;
 
 import javax.annotation.Nonnull;
 import javax.inject.Inject;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * Global item scorer to find similar items.
@@ -32,5 +34,36 @@ public class SimpleGlobalItemScorer extends AbstractGlobalItemScorer {
         // TODO score items in the domain of scores
         // each item's score is the sum of its similarity to each item in items, if they are
         // neighbors in the model.
+        for(VectorEntry e:scores.fast(VectorEntry.State.EITHER)){
+            long baseItemId = e.getKey();
+            List<ScoredId> neighbors = model.getNeighbors(baseItemId);
+            double sum = 0;
+            // test: if ref item only 77, then for each basteItemId we get score only for 77
+            for(long refItemId:items){
+                for(ScoredId neighbor:neighbors){
+                    if(neighbor.getId() == refItemId){
+                        sum += neighbor.getScore();
+                    }
+                }
+            }
+            scores.set(baseItemId, sum);
+
+
+
+
+
+            /*
+            long itemId = e.getKey();
+            List<ScoredId> neighbors = model.getNeighbors(itemId);
+            double sum = 0;
+            for(ScoredId neighbor:neighbors){
+                sum+=neighbor.getScore();
+            }
+            scores.set(itemId, sum);
+            */
+        }
+
+
+
     }
 }
